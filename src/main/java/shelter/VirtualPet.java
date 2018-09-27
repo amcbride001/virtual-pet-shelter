51c8b616517eabfc2ba524bbd229026204607f5e
Andrea McBride <31637885+amcbride001@users.noreply.github.com>
2018-09-26 21:42:26 -0400

    Add virtual-pet.java

---
+++ src/main/java/shelter/VirtualPet.java
@@ -0,0 +1,41 @@
+package shelter;
+
+public class VirtualPet {
+	private String tofu;
+	private String chucky;
+	private String hershey;
+	private String pet;
+	private String feed;
+	private String hydration;
+	private String goPlay;
+	private String name;
+
+	public VirtualPet(String string) {
+
+	}
+
+	public String getHydration() {
+		return hydration;
+	}
+
+	public String getFeedPets() {
+		return feed;
+	}
+
+	public String getGoPlay() {
+		return goPlay;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String takeHome() {
+		return name;
+
+	}
+}
